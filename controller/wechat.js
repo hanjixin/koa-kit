0326c309c6362c02aa346cfd29781e5f0f121c22
hanjixin <764671440@qq.com>
2019-12-25 22:36:01 +0800

    获取用户信息

--- controller/wechat.js
+++ controller/wechat.js
@@ -9,7 +9,7 @@ router.get('/getCode', async (ctx, next) => {
     });
   // await next();
   try {
-    const result = await new Promise(function(resolve, reject) {
+    let resultJson = await new Promise(function(resolve, reject) {
       request.get(
         `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${weChat.appid}&secret=${weChat.secret}&code=${ctx.query.code}&grant_type=authorization_code`,
         async (err, res) => {
@@ -20,21 +20,42 @@ router.get('/getCode', async (ctx, next) => {
         }
       );
     });
+    const result = JSON.parse(resultJson)
     if(!result.openid) {
       throw result
+    } else {
+      let resultJson = await new Promise(function(resolve, reject) {
+        request.get(
+          `https://api.weixin.qq.com/cgi-bin/user/info?access_token=${result.access_token}&openid=${result.openid}&lang=zh_CN
+
+          `,
+          async (err, res) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(res.body);
+          }
+        );
+      });
+      const UserInfo = JSON.parse(resultJson)
+      console.log(UserInfo)
+      var user = new User({
+        openid: UserInfo.openid,
+        unionid: '',
+        refresh_token: result.refresh_token,
+        userName: UserInfo.nickname,
+        password: 'String',
+        sex: UserInfo.sex + '',
+        age: '100',
+        avator: UserInfo.headimgurl
+      });
+      const dataRes = await user.save();
+      ctx.body = dataRes;
     }
-    var user = new User({
-      openid: result.openid,
-      unionid: '',
-      refresh_token: result.refresh_token,
-      userName: Date.now(),
-      password: 'String',
-      sex: 'String',
-      age: '',
-      avator: ''
-    });
-    const dataRes = await user.save();
-    ctx.body = dataRes;
+    // https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
+
+
+    
   } catch(err) {
     ctx.body = err
   }
